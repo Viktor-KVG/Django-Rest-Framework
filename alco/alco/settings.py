22641e0504cd381ab35b9ec9360107ad161ed894
Viktor-KVG <120111481+Viktor-KVG@users.noreply.github.com>
2023-06-08 17:50:51 +0300

    Add files via upload

--- alco/alco/settings.py
+++ alco/alco/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-MEDIA_ROOT = 'home/viteeek/DRT/media'
+MEDIA_ROOT = 'home/viteeek/media'
+STATIC_ROOT = 'home/viteeek/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
