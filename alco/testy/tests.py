2ae6acd6269dc1d331c99e278d1a4ab95002fd5e
Viktor-KVG <120111481+Viktor-KVG@users.noreply.github.com>
2023-06-09 23:00:26 +0300

    Add files via upload

--- alco/testy/tests.py
+++ alco/testy/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from testy.models import *
+from alco.urls import *
+
+class TestyTests(APITestCase):
+    def test_level(self):
+        response = self.client.get(Level.name_obj)
+        print(response.data)
+        # url = reverse('account-list')
+        # data = {'name': 'DabApps'}
+        # response = self.client.post(url, data, format='json')
+        # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # self.assertEqual(Account.objects.count(), 1)
+        # self.assertEqual(Account.objects.get().name, 'DabApps')
